27c681acf48091e64e71c74eab559a2a9d8388d3
nam <thnam.me@gmail.com>
2013-06-02 20:13:28 +0900

    Meshable test example

--- test.py
+++ test.py
@@ -15,13 +15,37 @@ for i in range(len(point)):
 point.Write(f)
 
 curve = gmshlib.LineList('c')
-#curve.Add(gmshlib.Line([point[0], point[1]], idtag = 200))
-#curve.Add(gmshlib.Line([point[1], point[2]]))
-#curve.Add(gmshlib.Line(point[2:4]))
-#curve._list.extend(gmshlib.MakePolyLines(point[0:4]))
 curve.PolyLines(point[0:4])
 curve.PolyLines(point[4:8])
+curve.Add(gmshlib.Line([point[4],point[0]]))
+curve.Add(gmshlib.Line([point[1],point[5]]))
+curve.Add(gmshlib.Line([point[6],point[2]]))
+curve.Add(gmshlib.Line([point[3],point[7]]))
 
 curve.Write(f)
 
+lineloop = gmshlib.ObjectList('ll')
+lineloop.Add(gmshlib.LineLoop(curve[0:4]))
+lineloop.Add(gmshlib.LineLoop(curve[4:8]))
+lineloop.Add(gmshlib.LineLoop([curve[0],curve[9],curve[4].Reverse(),curve[8]]))
+lineloop.Add(gmshlib.LineLoop([curve[1].Reverse(),curve[9],curve[5],curve[10]]))
+lineloop.Add(gmshlib.LineLoop([curve[2],curve[11],curve[6].Reverse(),curve[10]]))
+lineloop.Add(gmshlib.LineLoop([curve[3].Reverse(),curve[11],curve[7],curve[8]]))
 
+lineloop.Write(f)
+
+surface = gmshlib.ObjectList('sf')
+for i in range(len(lineloop)):
+  surface.Add(gmshlib.RuledSurface([lineloop[i]]))
+
+surface.Write(f)
+
+surfaceloop = gmshlib.ObjectList('sl')
+surfaceloop.Add(gmshlib.SurfaceLoop(surface))
+surfaceloop.Write(f)
+
+vol = gmshlib.ObjectList('vol')
+vol.Add(gmshlib.Volume(surfaceloop))
+vol.Add(gmshlib.PhysicalVolume([vol[0]]))
+
+vol.Write(f)
